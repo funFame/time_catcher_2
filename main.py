538f1016b181a83edf0e92cc4f3beb3167c34d56
Hiro <pythoneerHiro@gmail.com>
2023-04-28 23:31:28 +0530

    feat: logging
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-import os
+import logging
 import time
-import subprocess
 
+from conf import SLEEP_TIME
 from conf_git import *
 from run import run
-from conf import SLEEP_TIME
-
-checkout_repo(REMOTE_URL)
-git_config(GIT_USER,GIT_EMAIL)
-
-while True:
-    t = time.time()
-    with open("time.txt", 'w') as f1:
-        f1.write(str(t))
-
-    commit_msg = f"time: {t}"
-
-    commands = [
-        ["git", "add", "time.txt"],
-        ["git", "commit", "-m", commit_msg],
-        ["git", "push"]
-    ]
-
-    for cmd in commands:
-        run(cmd)
-
-    # print(f"commited  {commit_msg}")
-
-    time.sleep(SLEEP_TIME)
 
 
+def main():
+    checkout_repo(REMOTE_URL)
+    git_config(GIT_USER, GIT_EMAIL)
+
+    while True:
+        t = time.time()
+        with open("time.txt", 'w') as f1:
+            f1.write(str(t))
+
+        commit_msg = f"time: {t}"
+
+        commands = [
+            ["git", "add", "time.txt"],
+            ["git", "commit", "-m", commit_msg],
+            ["git", "push"]
+        ]
+
+        for cmd in commands:
+            res, err = run(cmd)
+
+            if res:
+                logging.info(" ".join(cmd), res)
+
+            if cmd[1] == "push":
+                if "rejected" in err:
+                    logging.critical(err)
+                    logging.warning("restarting the repo")
+                    return main()
+            if err:
+                logging.error(err)
+
+        time.sleep(SLEEP_TIME)
+        # print(f"commited  {commit_msg}")
+
+
+if __name__ == "__main__":
+    main()
--- run.py
+++ run.py
@@ -1,12 +1,20 @@
+import logging
 import subprocess
+from typing import Tuple
 
 
-def run(cmd: list):
-    res = subprocess.run(cmd, capture_output=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-                         )
-    print(res.stdout.decode('utf-8'))
+def run(cmd: list) -> Tuple[str, str]:
+    res = subprocess.run(
+        cmd,
+        capture_output=False,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output = res.stdout.decode('utf-8').strip()
+    logging.debug(output)
 
     err = res.stderr.decode("utf-8").strip()
     if err != "":
         # raise Exception(" ".join(cmd),err)
-        print("ERR", err)
+        logging.error(err)
+
+    return output, err
