6edd29b92bc1aec0a9f8588fe2bf0e82904df7b8
Hiro <pythoneerHiro@gmail.com>
2023-04-29 16:55:43 +0530

    fix; datetime instead of time
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
 import time
+from datetime import timedelta, datetime
 
 from conf import SLEEP_TIME
 from conf_git import *
 from conf_log import log
 from run import run
+
+
+def now(utc_offset=timedelta(hours=5, minutes=30)) -> datetime:  # default ist
+    now_utc = datetime.utcnow()
+    # utc_offset = datetime.timedelta(hours=5, minutes=30)
+    now_ist = now_utc + utc_offset
+    # print(f"UTC time: {now_utc}")
+    # print(f"IST time: {now_ist}")
+    return now_ist
 
 
 def main():
@@ -19,12 +29,15 @@ def main():
         ["git", "commit", "-m", "removed unnecessary files"]
     ]
 
+
     while True:
         t = time.time()
-        with open("time.txt", 'w') as f1:
-            f1.write(str(t))
+        now_ = now(timedelta(hours=5, minutes=30))
 
-        commit_msg = f"time: {t}"
+        with open("time.txt", 'w') as f1:
+            f1.write(str(now_))
+
+        commit_msg = f"time: {str(now_)}"
 
         commands = [
             ["git", "add", "time.txt"],
