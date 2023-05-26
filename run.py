b584cc20ab96c028126218ba8aab0750a83c5280
Hiro <pythoneerHiro@gmail.com>
2023-05-26 14:16:20 +0530

    improved: run
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>
    
    Took 14 minutes

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 import subprocess
-from typing import Tuple
+from typing import Tuple, Union
 
 from conf_log import log
 
 
-def run(cmd: list) -> Tuple[str, str]:
+def run(cmd: Union[list, str], **kwargs) -> Tuple[str, str]:
+    if type(cmd) == str:
+        cmd = cmd.split(" ")
     res = subprocess.run(
         cmd,
         capture_output=False,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
 
     output = res.stdout.decode('utf-8').strip()
-    log.debug(output)
+    log.info(output)
 
     err = res.stderr.decode("utf-8").strip()
     if err != "":
